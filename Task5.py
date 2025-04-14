d4ffcf296532cb643809a41a8e85f514e04c1b86
BALAKUMAR <bala8065@gmail.com>
2025-04-14 22:50:57 +0530

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,11 +1,11 @@
-﻿Name: Balakumar
-Email: bala8065@gmail.com
-Phno: 9790816791
-Batch code: PAT-C-WE-E-B7
+#﻿Name: Balakumar
+#Email: bala8065@gmail.com
+#Phno: 9790816791
+#Batch code: PAT-C-WE-E-B7
 
 
-Task 5
-1. Given a list of dictionaries each representing a person with name and age keys use lambda function to filter out people under 18 and the map the remaining peoples names to a new list
+#Task 5
+#1. Given a list of dictionaries each representing a person with name and age keys use lambda function to filter out people under 18 and the map the remaining peoples names to a new list
                 
 people = [
  {"name": "Bala", "age": 25},
@@ -37,11 +37,10 @@ adult_names1 = list(map(lambda person: person["name"], adults))
 print("Person below 18:", adult_names1)
 
 
-Output:
-Person above 18: ['Bala', 'Jeeva']
-Person below 18: ['kumar', 'Hema']
+#Output:
+#Person above 18: ['Bala', 'Jeeva'], Person below 18: ['kumar', 'Hema']
  
-2. Given a list of numbers use to reduce function and a lambda expression to calculate the product of all the numbers in the list 
+#2. Given a list of numbers use to reduce function and a lambda expression to calculate the product of all the numbers in the list 
         
         #import the reduce function 
 from functools import reduce
@@ -58,11 +57,10 @@ total = reduce(add, c)
 
 #print the value
 print(“Product of all the number:”,total)
-Output: 
-Product of all the number: 21
+#Output: Product of all the number: 21
 
 
-3. Write a list comprehension that creates a new list of squares of even numbers from a given list, using a lambda function to check for even numbers
+#3. Write a list comprehension that creates a new list of squares of even numbers from a given list, using a lambda function to check for even numbers
 
 
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -75,10 +73,10 @@ squareeven = [x**2 for x in numbers if (lambda x: x % 2 == 0)(x)]
 print(squareeven)
 
 
-Output:  [4, 16, 36, 64, 100]
+#Output:  [4, 16, 36, 64, 100]
 
 
-4. Write a lambda function to check if a given string is a number 
+#4. Write a lambda function to check if a given string is a number 
 
 
 is_num = lambda q: q.replace('.','',1).isdigit()
@@ -94,24 +92,18 @@ print(is_num1('-16.4'))
 print(is_num1('-24587.11'))
 
 
-Output: 
-True
-True
-False
-True
-False
-True
+#Output: 
+#True
+#True
+#False
+#True
+#False
+#True
+#Print checking numbers:
+#True
+#True
 
-
-Print checking numbers:
-True
-True
-
-
-
-
-5. Use a lambda function to extract the year month and day from a date time object 
-
+#5. Use a lambda function to extract the year month and day from a date time object 
 
 import datetime
 
@@ -119,28 +111,22 @@ import datetime
 # Create a lambda function to extract year, month, date, and time
 extract_datetime = lambda x: (x.year, x.month, x.day, x.time())
 
-
 # Get the current date and time
 current_datetime = datetime.datetime.now()
-
 
 # Extract year, month, date, and time using the lambda function
 year, month, day, time = extract_datetime(current_datetime)
 
-
 # Print the extracted values
-print("Year:", year)
-print("Month:", month)
-print("Day:", day)
-print("Time:", time)
+#print("Year:", year)
+#print("Month:", month)
+#print("Day:", day)
+#print("Time:", time)
 
-
-6. Create a lambda function to generate a fibonacci series up to n terms
+#6. Create a lambda function to generate a fibonacci series up to n terms
         
-                from functools import reduce
-
+    from functools import reduce
 
 fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2] range(n-2), [0, 1])
-
 
 print(fib(5))
